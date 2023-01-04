72072405f3625c118c32f8c0269d39940067f72f
Mubashar Nouman <mubashirrnouman@gmail.com>
2023-01-04 22:32:26 +0500

    4th January

---
+++ Mubashar Nouman/04_01_2023.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "This is my fourth day of code and I am sick \nBut I solved a problem on leetcode of Running Sum of Array." << endl;
+    return 0;
+}
